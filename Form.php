b4f25dfcccbed0a0adcf92ee0766422421d1b6ba
marinaindahp <62710340+marinaindahp@users.noreply.github.com>
2020-07-08 12:42:04 +0700

    Update and rename Form.html to Form.php

---
+++ Form.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Diri</title>
+</head>
+<body>
+	<h1 align="center">FORMULIR PERMOHONAN KARTU TANDA PENDUDUK (KTP)</h1>
+	<form method="post" action="Tabel.php">
+	<table style="border: 1px solid black; " align="center" width="700px">
+		<tr>
+			<td>
+				<label for="Provinsi">PEMERINTAH PROVINSI</label>
+			</td>
+			<td colspan="2">
+				:<input type="text" id="Provinsi" name="Provinsi" style="width: 50%" required="">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label for="Kota">PEMERINTAH KABUPATEN/KOTA</label>
+			</td>
+			<td colspan="2">
+				:<input type="text" id="Kota" name="Kota" style="width: 50%" required="">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label for="Nik">1. NIK</label>
+			</td>
+			<td colspan="2">
+				:<input type="number" id="Nik" name="Nik" style="width: 50%" required="">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label for="Nama">2. Nama Lengkap</label>
+			</td>
+			<td colspan="2">
+				:<input type="text" id="Nama" name="Nama" style="width: 50%" required="">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label for="tempat">3. Tempat, Tanggal Lahir</label>
+			</td>
+			<td colspan="2">
+				:<input type="text" id="tempat" name="Tempat" style="width: 50%" required="">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>4. Jenis Kelamin</label>
+			</td>
+			<td >
+				<input type="radio" id="Laki-Laki" name="Jeniss" value="Laki-laki" checked="" >
+				<label for="Laki-Laki">Laki-Laki</label>
+				<input type="radio" id="Perempuan" name="Jeniss" value="Perempuan">
+				<label for="Perempuan">Perempuan</label>
+			</td>
+		</tr>
+	
+		<tr>
+			<td>
+				<label>5. Alamat</label>
+			</td>
+			<td colspan="2">
+				:<input type="text" id="Alamat" name="Alamat" style="width: 50%" required="">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>	RT</label>
+			</td>
+			<td colspan="2">
+				:<input type="number" id="Rt" name="Rt" style="width: 50%" required="">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label>	RW</label>
+			</td>
+			<td colspan="2">
+				:<input type="number" id="Rw" name="Rw" style="width: 50%" required="">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label for="Desa">Kelurahan/Desa</label>
+			</td>
+			<td colspan="2">
+				:<input type="text" id="Desa" name="Desa" style="width: 50%" required="">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label for="Kecamatan">Kecamatan</label>
+				</td>
+			<td colspan="2">
+				:<input type="text" id="Kecamatan" name="Kecamatan" style="width: 50%" required="">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				6. Agama
+			</td>
+			<td colspan="2"><div class="select">
+				<select name="Agamaa" id="Agamaa">
+					<option>Islam</option>
+					<option>Kristen</option>
+					<option>Katholik</option>
+					<option>Budha</option>
+					<option>Hindu</option>
+					<option>Konghucu</option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				7. Status Perkawinan
+			</td>
+			<td colspan="2"><div class="select">
+				<select name="Status" id="Status">
+					<option>Belum Kawin</option>
+					<option>Kawin</option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				8. Pekerjaan
+			</td>
+			<td colspan="2">
+				<textarea cols="22" rows="4" name="Pekerjaan" required=""></textarea>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				9. Kewarganegaraan
+			</td>
+			<td colspan="2">
+				:<input type="text" id="Negara" name="Negara" style="width: 50%" required="">
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2" align="center" height="50px">
+			<input type="checkbox" name="s&k" required="">Data yang saya masukkan adalah benar adanya</td>
+		</tr>
+		<tr>
+			<td align="right">
+				<button type="submit">simpan</button>
+			</td>
+			<td align="left" colspan="2">
+				<button type="cancel">batal</button>
+			</td>
+		</tr>
+	</table>
+	</form>
+</body>
+</html>
